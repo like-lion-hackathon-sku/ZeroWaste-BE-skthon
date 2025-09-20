de15b01d36c528430d6b246d0382d2f6701899c3
김민호 <2022305011@skuniv.ac.kr>
2025-09-20 13:31:45 +0900

    delete restaurants part

--- src/restaurants/router/restaurants.router.js
+++ src/restaurants/router/restaurants.router.js
@@ -1,53 +1,9 @@
-// 위치: src / restaurants / router / restaurans.router.js
-import { Router } from "express";
-import {
-  searchRestaurantsCtrl,
-  getRestaurantDetailCtrl,
-} from "../controller/restaurants.controller.js";
-import { identifyAccessToken } from "../../auth/middleware/auth.middleware.js";
-import { handleGetRestaurantReviews } from "../controller/restaurant-reviews.controller.js";
+import express from "express";
 
-const r = Router({ mergeParams: true });
+const router = express.Router({ mergeParams: true });
 
-/* restaurantId 파라미터 검증 미들웨어
- * restaurantId가 숫자가 아닐경우 404 반환
- */
-function onlyDigits404(req, res, next) {
-  const { restaurantId } = req.params;
-  if (restaurantId !== undefined && !/^\d+$/.test(String(restaurantId))) {
-    return res
-      .status(404)
-      .json({ resultType: "FAILURE", error: "NOT_FOUND", success: null });
-  }
-  next();
-}
+router.get("/", (req, res) => {
+  res.send("restaurants");
+});
 
-/* 주변 식당 검색 라우터
- * 매서드: GET
- * 엔드포인트: api/restaurants/nearby
- */
-r.get("/nearby", searchRestaurantsCtrl);
-
-/* 식당 상세 조회 라우터
- * 매서드: GET
- * 엔드포인트: api/restaurants/:restaurantId/detail
- */
-r.get(
-  "/:restaurantId/detail",
-  onlyDigits404,
-  identifyAccessToken,
-  getRestaurantDetailCtrl,
-);
-
-/* 식당 리뷰 조회 라우터
- * 매서드: GET
- * 엔드포인트: api/restaurants/:restaurantId/reviews
- */
-r.get(
-  "/:restaurantId/reviews",
-  onlyDigits404,
-  identifyAccessToken,
-  handleGetRestaurantReviews,
-);
-
-export default r;
+export default router;
