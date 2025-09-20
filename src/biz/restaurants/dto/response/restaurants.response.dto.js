33e6f3dfe00eddd0561b72df229aef8b937dbd22
김민호 <2022305011@skuniv.ac.kr>
2025-09-20 19:39:44 +0900

    “식당파트”

--- src/biz/restaurants/dto/response/restaurants.response.dto.js
+++ src/biz/restaurants/dto/response/restaurants.response.dto.js
@@ -1,32 +1,32 @@
-// //  위치: src / biz / restaurants / dto / response / restaurants.response.dto.js
-// export class RestaurantResponseDto {
-//   constructor(entity) {
-//     this.id = entity.id;
-//     this.name = entity.name;
-//     this.category = entity.category;
-//     this.address = entity.address;
-//     this.telephone = entity.telephone;
-//     this.mapx = entity.mapx ?? null;
-//     this.mapy = entity.mapy ?? null;
-//     this.createdAt = entity.createdAt;
-//     this.updatedAt = entity.updatedAt;
-//   }
-// }
+//  위치: src / biz / restaurants / dto / response / restaurants.response.dto.js
+export class RestaurantResponseDto {
+  constructor(entity) {
+    this.id = entity.id;
+    this.name = entity.name;
+    this.category = entity.category;
+    this.address = entity.address;
+    this.telephone = entity.telephone;
+    this.mapx = entity.mapx ?? null;
+    this.mapy = entity.mapy ?? null;
+    this.createdAt = entity.createdAt;
+    this.updatedAt = entity.updatedAt;
+  }
+}
 
-// export class EnsureRestaurantResponseDto {
-//   constructor({ restaurantId, created }) {
-//     this.restaurantId = restaurantId;
-//     this.created = created;
-//   }
-// }
+export class EnsureRestaurantResponseDto {
+  constructor({ restaurantId, created }) {
+    this.restaurantId = restaurantId;
+    this.created = created;
+  }
+}
 
-// export const ok = (data) => ({
-//   resultType: "SUCCESS",
-//   error: null,
-//   success: data,
-// });
-// export const fail = (error) => ({
-//   resultType: "FAILURE",
-//   error,
-//   success: null,
-// });
+export const ok = (data) => ({
+  resultType: "SUCCESS",
+  error: null,
+  success: data,
+});
+export const fail = (error) => ({
+  resultType: "FAILURE",
+  error,
+  success: null,
+});
