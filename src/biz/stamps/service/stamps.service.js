5d4191cb6d15cb8be5254d8b5a3b92b08812b930
ほし <43630612+hoshi2710@users.noreply.github.com>
2025-09-24 20:39:01 +0900

    ♻️Refactor: 보유 스탬프 목록, 스탬프 사용시 식당 ID 반환하도록 수정 #121

--- src/biz/stamps/service/stamps.service.js
+++ src/biz/stamps/service/stamps.service.js
@@ -2,7 +2,7 @@ import { verifyStampsResponseDto } from "../dto/response/stamps.response.dto.js"
 import { expireStampCode } from "../repository/stamps.repository.js";
 
 export const verifyStamps = async (data) => {
-  const customerId = await expireStampCode(data);
+  const results = await expireStampCode(data);
   if (customerId === -1) throw new Error("유효하지 않은 쿠폰 번호 입니다.");
-  return verifyStampsResponseDto(customerId);
+  return verifyStampsResponseDto(results);
 };
